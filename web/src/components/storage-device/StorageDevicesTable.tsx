092a9d363d962592359d37fd04c37ec03cf48152
RiCheansss <pavelcutov@gmail.com>
2025-06-03 10:01:00 +0700

    Fix for Vite

--- web/src/components/storage-device/StorageDevicesTable.tsx
+++ web/src/components/storage-device/StorageDevicesTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { DataGrid, GridColDef, GridRowsProp, GridActionsCellItem } from '@mui/x-data-grid';
 import { useGetStorageDevicesQuery, useDeleteStorageDeviceMutation } from '../../store/api/apiStorageDevice';
 
